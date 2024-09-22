1afeee76a60361da0ffb7689c5f00084e3e633aa
Nicole L <158861083+Nicole-CY@users.noreply.github.com>
2024-09-22 22:45:13 +1000

    remove comment

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-const express = require('express')
-const router = require('./router')
-const cors = require('cors')
+const express = require("express");
+const router = require("./router");
+const cors = require("cors");
 
-const app = express() //create a web server
-app.use(express.json()) //JSON 解析中间件 parse json request bodies
-app.use(cors()) // Enable CORS for all routes
-app.use('/api',router) // Use router middleware
+const app = express(); //create a web server
+app.use(express.json()); //parse json request bodies
+app.use(cors()); // Enable CORS for all routes
+app.use("/api", router); // Use router middleware
 
-const port = 8000
-app.listen(port,function(){
-    console.log(`Server is running on port ${port}`)
-    console.log(`http://localhost:${port}`)
-})
-
-
+const port = 8000;
+app.listen(port, function () {
+  console.log(`Server is running on port ${port}`);
+  console.log(`http://localhost:${port}`);
+});
